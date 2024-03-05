115284cdbe88cfde24a6be244a030d59ec309bc1
Roxana <ms.roxanarahimi@gmail.com>
2024-03-05 20:02:36 +0330

    change

---
+++ src/components/Carousel.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="home" class="carousel carousel-dark slide scrollElement" data-bs-ride="carousel" data-bs-pause="false">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    </div>
+    <div class="carousel-inner h-100">
+      <div class="carousel-item px-lg-3 active h-100" data-bs-interval="3000">
+        <div class="row align-self-center d-none d-lg-flex flex-row-reverse h-100">
+          <div class="col-lg-7 h-100 d-grid">
+            <img src="/img/slide1.png" class="img-fluid align-self-center" alt="">
+          </div>
+          <div class="col-lg-5 h-100 d-grid"  style="background:url('/img/slideBg.png') top right no-repeat; background-size: 100%">
+            <div class="carousel-text align-self-center text-light  fw-bolder mt-4">
+              <p class="ps-4">کوپاکافه</p>
+              <p class="ps-4">طعم واقعی قهوه</p>
+            </div>
+          </div>
+        </div>
+        <div class="row align-self-center d-lg-none h-100 p-2" style="background:url('/img/slideBg2.png') bottom right no-repeat; background-size: 100%;">
+          <div class="col-12 d-grid">
+            <img src="/img/slide1.png" class="img-fluid align-self-center me-auto" alt="">
+          </div>
+          <div class="col-12 d-grid">
+            <div class="carousel-text align-self-end text-light  fw-bolder mt-4 pb-5 mb-5" style="font-size:35px">
+              <p class="ps-4">کوپاکافه</p>
+              <p class="ps-4">طعم واقعی قهوه</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item px-lg-3 h-100" data-bs-interval="3000">
+        <div class="row align-self-center d-none d-lg-flex flex-row-reverse h-100">
+          <div class="col-lg-7 h-100 d-grid">
+            <img src="/img/slide2.png" class="img-fluid align-self-center me-auto" alt="">
+          </div>
+          <div class="col-lg-5 h-100 d-grid"  style="background:url('/img/slideBg.png') top right no-repeat; background-size: 100%">
+            <div class="carousel-text align-self-center text-light pb-5  fw-bolder mt-4">
+              <p class="ps-4">کوپاکافه</p>
+              <p class="ps-4">دنیای تنوع و کیفیت</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="row align-self-center d-lg-none h-100 p-2" style="background:url('/img/slideBg2.png') bottom right no-repeat; background-size: 100%;">
+          <div class="col-12 d-grid">
+            <img src="/img/slide2.png" class="img-fluid align-self-center" alt="">
+          </div>
+          <div class="col-12 d-grid"  >
+            <div class="carousel-text align-self-end text-light  fw-bolder mt-4 mb-5 pb-5" style="font-size:35px">
+              <p class="ps-4">کوپاکافه</p>
+              <p class="ps-4">دنیای تنوع و کیفیت</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Carousel"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="products" class=" scrollElement row justify-content-center min-vh-100" style="padding-top: 70px">
+
+    <div class="text-center mb-5">
+      <div class="rounded-3 d-inline-block p-1" style="border: 1px solid #004400">
+        <h1 class="my-bg d-inline-block mx-auto p-4 rounded-3 mb-0">محصولات کوپاکافه</h1>
+      </div>
+
+    </div>
+    <div class="row col-10  ">
+      <div class="d-none d-lg-block col-5"></div>
+      <div class="row mb-5 px-2 col-lg-7 flex-row-reverse">
+        <div class="col-lg-8 p-0 d-flex justify-content-between mb-4 px-lg-4 mb-lg-0">
+          <div v-for="(index,item) in 5" :key="index" class="category " style="padding:3px; border: 1px #004400 solid">
+            <div :class="{'my-bg': index == 1} " class="w-100 h-100 rounded " style=" background-color: #e2e2e2">
+              <i class="bi bi-cup-hot"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 d-grid">
+          <p class="align-self-end m-0 ">قهوه فوری</p>
+        </div>
+
+      </div>
+
+      <div class="row flex-row-reverse">
+        <div class="col-lg-7">
+          <div class="row  d-none d-lg-flex">
+            <div v-for="item in 6" class="col-4 px-3 mb-4">
+              <div class="card rounded-4">
+                <div class="card-body pt-5">
+                  <div class="card-img mb-3">
+                    <img src="/img/cc.webp" class="img-fluid" alt="">
+                  </div>
+                  <div class="card-title">
+                    <p>قهوه فوری</p>
+                    <small>بسته 500 گرمی</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=" d-lg-none w-100" style=" overflow-x: scroll">
+            <div class="d-flex" style="min-width: 100%;">
+              <div v-for="item in 6" class=" col-6 px-2 mb-3" style="width: 180px !important">
+                <div class="card rounded-3 w-100">
+                  <div class="card-body pt-4">
+                    <div class="card-img mb-3">
+                      <img src="/img/cc.webp" class="img-fluid" alt="">
+                    </div>
+                    <div class="card-title">
+                      <p>قهوه فوری</p>
+                      <small>بسته 500 گرمی</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5">
+          <div class="card border-0 rounded-4">
+            <div class="card-body">
+              <div class="card-img mb-3">
+                <img src="/img/cc.webp" class="img-fluid" alt="">
+              </div>
+              <div class="card-title">
+                <p>قهوه فوری</p>
+                <small>بسته 500 گرمی</small>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Products"
+}
+</script>
+
+<style scoped>
+
+</style>
