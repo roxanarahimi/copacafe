4816c6fd89d4589126df6d7a4ddcf295bd03d860
Roxana <ms.roxanarahimi@gmail.com>
2024-03-10 17:20:05 +0330

    change

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,22 +1,20 @@
 <template>
-  <div id="home"  class="carousel carousel-dark slide scrollElement" data-bs-ride="carousel" data-bs-pause="false">
+  <div id="home" v-if="data?.length"  class="carousel carousel-dark slide scrollElement" data-bs-ride="carousel" data-bs-pause="false">
     <div class="carousel-indicators">
-      <button type="button" data-bs-target="#home" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#home" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button v-for="(item,index) in data" :key="index" type="button" data-bs-target="#home" :data-bs-slide-to="index" :class="{'active': index == 0}"  aria-current="true" :aria-label="'Slide '+index"></button>
     </div>
     <div class="carousel-inner h-100">
-      <div class="carousel-item  active h-100" data-bs-interval="3000">
+      <div v-for="(item,index) in data" :key="index" class="carousel-item  h-100" :class="{'active': index == 0}" data-bs-interval="3000">
         <div class="row align-self-center d-none d-lg-flex flex-row-reverse h-100">
           <div class="col-lg-7 h-100 d-grid">
-            <img src="/img/slide1.png" class="img-fluid align-self-center me-auto pe-lg-4" alt="">
+            <img :src="url+item.image" class="img-fluid align-self-center me-auto pe-lg-4" alt="">
           </div>
           <div class="col-lg-5 h-100 d-grid"  style="background:url('/img/slideBg.png') top right no-repeat; background-size: 100%">
             <div class="carousel-text align-self-center text-light pb-5  fw-bolder mt-4">
-              <p class="ps-4">کوپاکافه</p>
-              <p class="ps-4">طعم واقعی قهوه</p>
+              <p class="ps-4">{{ item.title}}</p>
+              <p class="ps-4">{{  item.subTitle }}</p>
             </div>
           </div>
-
         </div>
         <div class="row align-self-center d-lg-none h-100 p-2" style="background:url('/img/slideBg2.png') bottom right no-repeat; background-size: 100%;">
           <div class="col-12 d-grid">
@@ -26,33 +24,6 @@
             <div class="carousel-text align-self-end text-light  fw-bolder mt-4 mb-5 pb-5" style="font-size:35px">
               <p class="ps-4">کوپاکافه</p>
               <p class="ps-4">طعم واقعی قهوه</p>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-      <div class="carousel-item  h-100" data-bs-interval="3000">
-        <div class="row align-self-center d-none d-lg-flex flex-row-reverse h-100">
-          <div class="col-lg-7 h-100 d-grid">
-            <img src="/img/slide2.png" class="img-fluid align-self-center me-auto pe-lg-4" alt="">
-          </div>
-          <div class="col-lg-5 h-100 d-grid"  style="background:url('/img/slideBg.png') top right no-repeat; background-size: 100%">
-            <div class="carousel-text align-self-center text-light pb-5  fw-bolder mt-4">
-              <p class="ps-4">کوپاکافه</p>
-              <p class="ps-4">دنیای تنوع و کیفیت</p>
-            </div>
-          </div>
-
-        </div>
-        <div class="row align-self-center d-lg-none h-100 p-2" style="background:url('/img/slideBg2.png') bottom right no-repeat; background-size: 100%;">
-          <div class="col-12 d-grid">
-            <img src="/img/slide2.png" class="img-fluid align-self-center" alt="">
-          </div>
-          <div class="col-12 d-grid"  >
-            <div class="carousel-text align-self-end text-light  fw-bolder mt-4 mb-5 pb-5" style="font-size:35px">
-              <p class="ps-4">کوپاکافه</p>
-              <p class="ps-4">دنیای تنوع و کیفیت</p>
             </div>
           </div>
 
@@ -73,8 +44,26 @@
 </template>
 
 <script>
+import {useStore} from "vuex";
+import {computed, onBeforeMount, onMounted} from "vue";
+
 export default {
-  name: "Carousel"
+  name: "Carousel",
+  setup(){
+    const store = useStore();
+    const url = store.state.panelUrl;
+    const getData = () => {
+      store.commit('getSlides');
+    };
+    onMounted(()=>{
+      getData();
+    })
+
+    return{
+      data: computed(()=>store.state.slides),
+      getData,store, url
+    }
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,57 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    panelUrl: 'https://panel.copacaffee.webagent.ir',
+    slides: null,
+    productsCats: null,
+    products: null,
+    product: null,
   },
   mutations: {
+    getSlides(state) {
+      axios.get(state.panelUrl + '/api/slide')
+          .then((response) => {
+            state.slides = response.data;
+          }).catch();
+    },
+   getProductCats(state) {
+      axios.get(state.panelUrl + '/api/category/product')
+          .then((response) => {
+            state.productsCats = response.data;
+          }).catch();
+    },
+    getProducts(state,id) {
+      axios.get(state.panelUrl + '/api/product?cat='+id)
+          .then((response) => {
+            state.products = response.data;
+          }).catch();
+    },
+    getProduct(state, id) {
+      axios.get(state.panelUrl + '/api/product/' + id)
+          .then((response) => {
+            state.product = response.data.product;
+          }).then((response) => {
+        // if (state.product.features) {
+        //     state.productFeatures = [];
+        //     for (let i = 0; i < JSON.parse(state.product.features).length; i++) {
+        //         state.productFeatures.push(JSON.parse(state.product.features)[i]);
+        //     }
+        // }
+      }).catch();
+    }
   },
   actions: {
-  },
-  modules: {
+    getSlides(context) {
+      context.commit('getSlides');
+    },
+    getProductCats(context) {
+      context.commit('getProductCats');
+    },
+    getProducts(context) {
+      context.commit('getProducts');
+    },
+    getProduct(context) {
+      context.commit('getProduct');
+    },
   }
 })
